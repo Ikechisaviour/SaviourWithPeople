88290e329d39edc6a41af4ddba4ef240e1f4bf49
Ikechisaviour <43084681+Ikechisaviour@users.noreply.github.com>
2021-11-04 20:35:59 +0900

    Update test.sol

--- test.sol
+++ test.sol
@@ -1,1 +1,13 @@
-pragma ^0.8.1
+pragma solidity ^0.8.0;
+
+contract addressExample{
+    address public myAddress;
+    
+    function setAddress(address _myAddress) public {
+        myAddress = _myAddress;
+    }
+    
+    function getBalance() public view returns(uint){
+        return myAddress.balance;
+    }
+}
